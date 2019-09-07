266054bafc8fe77ea5eb9e987b2b1c51f0eca30d
rockian <mbappec@gmail.com>
2019-09-06 22:51:32 -0500

    Initial project

---
+++ src/main/java/com/afwin/pppetclinic/PpPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.afwin.pppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpPetClinicApplication.class, args);
+    }
+
+}
